25cf2939e151ce50a6eda12f44d947c087ffc793
Souptar <jamjoy0@gmail.com>
2021-01-23 13:59:05 +0000

    Add scandal and scandalDates arrays

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,48 @@
 var trump = document.getElementById("trump"); 
 var barrier = document.getElementById("barrier");
+let scandals = [
+    '"Mexicans = criminals"',
+    'Muslim ban',
+    'Jeb Bush',
+    'Ted Cruz',
+    'Access Hollywood tape',
+    'Presidential debates',
+    'Election 2016',
+    'Alternative facts', 
+    'Scaramucci', 
+    '"little rocket man"', 
+    'Stormy Daniels', 
+    'Kids in cages', 
+    'Brett Kavanaugh', 
+    'Ukraine phone call', 
+    'Buying Greenland', 
+    'Weather map Sharpie', 
+    'Impeachment #1', 
+    'COVID-19 Response', 
+    'Amy Coney Barrett'
+]
+let scandalDates = [
+    "June 2015",
+    "December 2015",
+    "February 2016",
+    "May 2016",
+    "October 2016",
+    "October 2016",
+    "November 2016",
+    "January 2017",
+    "July 2017",
+    "September 2017",
+    "January 2018",
+    "June 2018",
+    "September 2018",
+    "July 2019",
+    "August 2019",
+    "September 2019",
+    "December 2019",
+    "2020",
+    "October 2020",
+]
+
 
 function start(){
     barrier.classList.add("startMoving"); // when start button pressed, barrier starts moving
