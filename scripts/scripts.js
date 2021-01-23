d80705fbab45349b364fc36ad8737ee6f6e05c0d
Souptar <jamjoy0@gmail.com>
2021-01-23 13:32:37 +0000

    Add start button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,11 @@
 var trump = document.getElementById("trump"); 
 var barrier = document.getElementById("barrier");
+
+function start(){
+    barrier.classList.add("startMoving"); // when start button pressed, barrier starts moving
+    document.getElementById("startButton").style.display = "none" // hides start button 
+}
+
 function jump(){
     if(trump.classList == "animate"){return} // if 'animate class is already applied to trump div, stop
     trump.classList.add("animate"); // otherwise add animate class
@@ -12,9 +18,10 @@ var checkDead = setInterval(function() {
     let trumpTop = parseInt(window.getComputedStyle(trump).getPropertyValue("top")); //evaluate top position of Trump and parse as integer to remove 'px' from result
     let barrierLeft = parseInt(window.getComputedStyle(barrier).getPropertyValue("left")); //evaluate left position of Barrier and parse as integer to remove 'px' from result
     if(barrierLeft<95 && barrierLeft>-95 && trumpTop>=130){ //only true if Trump and Barrier are touching
-        barrier.style.animation = "none"; // stop animation
+        barrier.classList.remove("startMoving"); // stop animation
         alert("Game Over"); //placeholder alert
-        barrier.style.animation = "barrier 2s infinite linear"; // after alert is dismissed starts again
+        document.getElementById("startButton").style.display = "inline-block" // start button reappears
+
     }
 }, 10);
 
